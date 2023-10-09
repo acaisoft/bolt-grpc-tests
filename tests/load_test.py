6be91b0c1f1289b94699c06982b33af741d2614e
Jerzy Goebel <jerzy.goebel@acaisoft.com>
2023-10-09 13:25:53 +0200

    - fallback for forcing protocol on Bolt fe

--- tests/load_test.py
+++ tests/load_test.py
@@ -3,6 +3,7 @@ from locust import task
 import sys
 import os
 from pathlib import Path
+from urllib.parse import urlparse
 
 current_directory = Path(os.path.dirname(os.path.abspath(__file__))).parent.absolute()
 proto_directory = os.path.join(current_directory, 'pb')
@@ -13,8 +14,11 @@ from pb import api_pb2_grpc, api_pb2
 
 class ApiGrpcUser(grpc_user.GrpcUser):
     port = os.getenv("PORT", "50051")
-    target_host = os.getenv("HOST", "34.29.250.204")
-    host = f"{target_host}:{port}"
+    target_host = urlparse(os.getenv("HOST", "34.29.250.204"))
+    if target_host.scheme == "":
+        host = f"{target_host.path}:{port}"
+    else:
+        host = f"{target_host.netloc}:{port}"
     stub_class = api_pb2_grpc.ApiStub
 
     @task
